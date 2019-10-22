bede484e548bbd1b7c9bc516401d886a88dc275b
mattacer <34256656+mattacer@users.noreply.github.com>
2019-10-21 20:33:47 -0700

    added gameMessage to rouletteGame object

--- commands/User/currentgame.js
+++ commands/User/currentgame.js
@@ -15,7 +15,7 @@ module.exports = {
             if(args && args[0] && args[0] === "end"){
                 endGame.execute(message, null);
             } else {
-                message.author.send(endGame.messageToLink(game.gameMessage)).catch(console.error);
+                message.author.send(`You are currently playing ${endGame.messageToLink(game.gameMessage)}.`).catch(console.error);
             }
         } else {
             message.author.send("You are not currently in a game.").catch(console.error);
